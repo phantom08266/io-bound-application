d71c72f43e1cd998620de2c5b5769c0748197f65
phantom08266 <phantom08266@gmail.com>
2021-07-07 12:18:49 +0900

    feat: db에서 es사용하도록 설정

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -20,8 +20,6 @@ public class PostController {
     Producer producer;
     @Autowired
     ObjectMapper objectMapper;
-    @Autowired
-    PostCacheService postCacheService;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
@@ -31,26 +29,9 @@ public class PostController {
         return post;
     }
 
-    // 2. 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        if (page.equals(1)){
-            return postCacheService.getFirstPostPage();
-        }else{
-            return postRepository.findAll(
-                    PageRequest.of(page -1, PAGE_SIZE, Sort.by("id").descending())
-            );
-        }
-    }
-    
-    // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id){
-        return postRepository.findById(id).get();
-    }
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
     @GetMapping("/search")
     public List<Post> findPostsByContent(@RequestParam String content){
-        return postRepository.findByContentContains(content);
+        return postRepository.findByContent(content);
     }
 }
